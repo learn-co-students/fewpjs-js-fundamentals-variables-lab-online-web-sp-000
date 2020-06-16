53f200e63f01ed7fe0b97fc588edbda972ae7b7d
Vesna Vucinic <vesna.vucinic68@gmail.com>
2020-06-16 13:30:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file!
+// let companyName = 'Scuber'
+// const companyname = 'Scuber'
+// let mostProfitableNeighborhood = 'Chelsea'
+// mostProfitableNeighborhood = 'Upper West Side'
+// let companyCeo = 'Susan Smith'
+// companyCeo = 'Lauren Hart'
+
+  
+const companyName = 'Scuber'
+let mostProfitableNeighborhood = 'Chelsea'
+let companyCeo = 'Susan Smith'
